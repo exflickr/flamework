2bfea7dfa305478ab8bc437b218187f2759d8913
Cal Henderson <cal@iamcal.com>
2010-08-22 17:07:45 -0700

    stylized logging - a color per log type. output stuff when debug flag is passed

--- include/lib_log.php
+++ include/lib_log.php
@@ -3,15 +3,35 @@
 	# $Id$
 	#
 
+	$GLOBALS[log_colors] = array(
+		'db'		=> '#eef',
+		'smarty'	=> '#efe',
+	);
+
 	function log_fatal($msg){
 
-		die($msg);
+		die("FATAL ERROR: ".$msg);
 	}
 
 	function log_error($msg){
+		if (!$_GET[debug]) return;
+
 	}
 	
 	function log_notice($type, $msg, $time=-1){
+		if (!$_GET[debug]) return;
+
+
+		$color = $GLOBALS[log_colors][$type] ? $GLOBALS[log_colors][$type] : '#eee';
+
+		echo "<div style=\"background-color: $color; margin: 1px 1px 0 1px; border: 1px solid #000; padding: 4px; text-align: left\">";
+
+		echo "$msg";
+		if ($time > -1){
+			echo" ($time ms)";
+		}
+
+		echo "</div>\n";
 	}
 
 ?>
