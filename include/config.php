aa6b007f5fb8cac736f22803e20aacb0ab7b7f2c
straup <straup@gmail.com>
2010-09-08 23:37:16 +0000

    comments; start of auth/login configs; define smarty template paths

--- include/config.php
+++ include/config.php
@@ -1,6 +1,10 @@
 <?
 	$GLOBALS[cfg] = array();
 
+	#
+	# Things you'll certainly need to tweak
+	#
+
 	$GLOBALS[cfg][db_main] = array(
 		'host'	=> 'localhost',
 		'user'	=> 'root',
@@ -26,7 +30,24 @@
 	$GLOBALS[cfg][abs_root_url]		= 'http://www.ourapp.com/';
 	$GLOBALS[cfg][safe_abs_root_url]	= $GLOBALS[cfg][abs_root_url];
 
-	$GLOBALS[cfg][smarty_compile] = 1;
+	$GLOBALS[cfg][smarty_template_dir] = FLAMEWORK_INCLUDE_DIR.'/../templates/';
+	$GLOBALS[cfg][smarty_compile_dir] = FLAMEWORK_INCLUDE_DIR.'/../templates_c/';
 
+	#
+	# Things you may need to tweak
+	#
+
+	$GLOBALS[cfg][auth_cookie_domain] = parse_url($GLOBALS[cfg][abs_root_url], 1);
+	$GLOBALS[cfg][auth_cookie_name] = 'a';
+
+	#
+	# Things you can probably not worry about
+	#
+
+	$GLOBALS[cfg][user] = array();
+	$GLOBALS[cfg][user_ok] = 0;
+
+	$GLOBALS[cfg][smarty_compile] = 1;
 	$GLOBALS[cfg][http_timeout] = 3;
+
 ?>
