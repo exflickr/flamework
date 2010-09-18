af3b29a3280afa0f68bc3a3bb22553619ed6ea6c
Cal Henderson <cal@iamcal.com>
2010-09-18 13:58:11 +0800

    whitespace. escape message for html handler

--- include/lib_log.php
+++ include/lib_log.php
@@ -3,7 +3,7 @@
 	# $Id$
 	#
 
-	$GLOBALS[log_colors] = array(
+	$GLOBALS[log_html_colors] = array(
 		'db'		=> '#eef',
 		'smarty'	=> '#efe',
 		'http'		=> '#ffe',
@@ -13,7 +13,14 @@
 		'notice'	=> array('html'),
 		'error'		=> array('error_log'),
 	);
-	
+
+
+	###################################################################################################################
+
+	#
+	# public api
+	#
+
 	function log_fatal($msg){
 		die("FATAL ERROR: ".$msg);
 	}
@@ -21,51 +28,60 @@
 	function log_error($msg){
 		_log_dispatch('error', $msg);
 	}
-	
+
+
 	function log_notice($type, $msg, $time=-1){
 		_log_dispatch('notice', $msg, array(type => $type, time => $time));
 	}
-	
-	
-	function _log_dispatch($level, $msg, $more = array()) {
-		if ($GLOBALS[log_handlers][$level]) {
-			foreach ($GLOBALS[log_handlers][$level] as $handler) {
+
+
+	function _log_dispatch($level, $msg, $more = array()){
+
+		if ($GLOBALS[log_handlers][$level]){
+
+			foreach ($GLOBALS[log_handlers][$level] as $handler){
+
 				call_user_func("_log_handler_$handler", $level, $msg, $more);
 			}
 		}
 	}
-	
-	function _log_handler_error_log($level, $msg, $more = array()) {
+
+	###################################################################################################################
+
+	#
+	# log handlers
+	#
+
+	function _log_handler_error_log($level, $msg, $more = array()){
 		$page = $GLOBALS[HTTP_SERVER_VARS][REQUEST_URI];
-		
-		if ($more[type]) {
+
+		if ($more[type]){
 			$msg = "[$more[type]] $msg";
 		}
-		
+
 		$msg = str_replace("\n", ' ', $msg);
-		
+
 		error_log("[$level] $msg ($page)");
 	}
-	
-	function _log_handler_html($level,$msg, $more = array()) {
+
+	function _log_handler_html($level, $msg, $more = array()){
 		if (!$_GET[debug]) return;
-		
+
 		$type = $more[type] ? $more[type] : '';
-		
-		$color = $GLOBALS[log_colors][$type] ? $GLOBALS[log_colors][$type] : '#eee';
+
+		$color = $GLOBALS[log_html_colors][$type] ? $GLOBALS[log_html_colors][$type] : '#eee';
 
 		echo "<div style=\"background-color: $color; margin: 1px 1px 0 1px; border: 1px solid #000; padding: 4px; text-align: left\">";
 
-		if ($type) {
-			$msg = "[$type] $msg";
-		}
-		
-		echo "$msg";
-		if ($more[time] > -1){
-			echo " ($more[time] ms)";
-		}
+		if ($type) echo "[$type] ";
+
+		echo HtmlSpecialChars($msg);
+
+		if ($more[time] > -1) echo " ($more[time] ms)";
 
 		echo "</div>\n";
-		
+
 	}
+
+	###################################################################################################################
 ?>
