511da2cd2cb44bd3b26586da73c67e19b06b880a
Cal Henderson <cal@iamcal.com>
2010-09-18 14:21:32 +0800

    test for logging

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
 	include('include/init.php');
 
 
+	#
+	# this is so we can test the logging output
+	#
+
+	if ($_GET[log_test]){
+		log_error("This is an error!");
+		log_fatal("Fatal error!");
+	}
+
 
 	#
 	# output
