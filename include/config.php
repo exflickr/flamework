7feb8b9c17d6ba186b768a0a46401dc4c68dcd1c
straup <straup@gmail.com>
2010-10-05 00:02:59 +0000

    clean up remaining diffs

--- include/config.php
+++ include/config.php
@@ -1,5 +1,4 @@
 <?
-<<<<<<< HEAD
 
 	#
 	# $Id$
@@ -11,10 +10,8 @@
 	# Things you'll certainly need to tweak
 	#
 
-=======
 	$GLOBALS['cfg'] = array();
 
->>>>>>> 7f3ca545bc4a9243e7b3c8849b4c8f1138928c38
 	$GLOBALS['cfg']['db_main'] = array(
 		'host'	=> 'localhost',
 		'user'	=> 'root',
@@ -23,26 +20,29 @@
 		'auto_connect' => 0,
 	);
 
+
 	$GLOBALS['cfg']['db_users'] = array(
+
 		'host' => array(
 			1 => 'localhost',
 			2 => 'localhost',
 		),
+
 		'user' => 'root',
 		'pass' => 'root',
+
 		'name' => array(
 			1 => 'user1',
 			2 => 'user2',
 		),
 	);
 
-
 	$GLOBALS['cfg']['abs_root_url']		= 'http://www.ourapp.com/';
 	$GLOBALS['cfg']['safe_abs_root_url']	= $GLOBALS['cfg']['abs_root_url'];
-<<<<<<< HEAD
 
 	$GLOBALS['cfg']['smarty_template_dir'] = dirname(__FILE__) . '/../templates/';
 	$GLOBALS['cfg']['smarty_compile_dir'] = dirname(__FILE__) . '/../templates_c/';
+
 
 	# No, seriously. Change this...
 
@@ -63,14 +63,9 @@
 	$GLOBALS['cfg']['user_ok'] = 0;
 
 	$GLOBALS['cfg']['smarty_compile'] = 1;
+
 	$GLOBALS['cfg']['http_timeout'] = 3;
 
-?>
-=======
-
-	$GLOBALS['cfg']['smarty_compile'] = 1;
 	$GLOBALS['cfg']['check_notices'] = 1;
 
-	$GLOBALS['cfg']['http_timeout'] = 3;
 ?>
->>>>>>> 7f3ca545bc4a9243e7b3c8849b4c8f1138928c38
