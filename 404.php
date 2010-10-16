998096674fbab251dda4f0fbf4a34f70f26bdd60
Cal Henderson <cal@iamcal.com>
2010-10-16 13:42:30 -0700

    patch reformatting

--- 404.php
+++ 404.php
@@ -1,26 +1,22 @@
 <?php
-	#
-	# we don't want to connect to the database here
-	#
-	$_GET['no_db'] = true;
-
 	include('include/init.php');
+
 
 	#
 	# This is where you redirect users to a different page incase you've moved stuff or you can use .htaccess
 	# do this for you.
 	#
-	if (
-		$_SERVER['REQUEST_URI'] == '/home'
-	) {
-		Header("Location: /");
+
+	if ($_SERVER['REQUEST_URI'] == '/home'){
+
+		header("Location: /");
 		exit;
 	}
+
 
 	#
 	# output
 	#
-	Header("HTTP/1.1 404 Not Found")
 
 	$smarty->display('page_404.txt');
 ?>
