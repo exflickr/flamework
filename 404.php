3831add691c65206b70d462d3cb736281f845d2f
Cal Henderson <cal@iamcal.com>
2010-10-16 14:09:59 -0700

    added full 404 handling logic

--- 404.php
+++ 404.php
@@ -1,22 +1,5 @@
 <?php
 	include('include/init.php');
 
-
-	#
-	# This is where you redirect users to a different page incase you've moved stuff or you can use .htaccess
-	# do this for you.
-	#
-
-	if ($_SERVER['REQUEST_URI'] == '/home'){
-
-		header("Location: /");
-		exit;
-	}
-
-
-	#
-	# output
-	#
-
-	$smarty->display('page_404.txt');
+	error_404();
 ?>
