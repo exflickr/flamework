823c63be1da40b5c53a2f241db7f20d9e944d7a4
Aaron Straup Cope <straup@gmail.com>
2010-10-17 05:09:28 -0700

    moved mcrypt stuff to lib_crypto_mcrypt

--- include/lib_crypto.php
+++ include/lib_crypto.php
@@ -4,28 +4,6 @@
 	# $Id$
 	#
 
-	#################################################################
-
-	$GLOBALS['crypto_td'] = MCRYPT_RIJNDAEL_256;
-
-	#################################################################
-
-	function crypto_encrypt($data, $key){
-
-		$enc = mcrypt_encrypt($GLOBALS['crypto_td'], $key, $data, MCRYPT_MODE_ECB);
-		return base64_encode($enc);
-	}
-
-	#################################################################
-
-	function crypto_decrypt($enc_b64, $key){
-
-		$enc = base64_decode($enc_b64);
-		$dec = mcrypt_decrypt($GLOBALS['crypto_td'], $key, $enc, MCRYPT_MODE_ECB);
-
-		return trim($dec);
-	}
-
-	#################################################################
+	loadlib("crypto_mcrypt");
 
 ?>
