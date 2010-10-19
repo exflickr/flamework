a79f15353848e7b955c8b24e7cb78ca673af6fae
Cal Henderson <cal@iamcal.com>
2010-10-18 22:40:46 -0700

    formatting

--- checkcookie.php
+++ checkcookie.php
@@ -1,17 +1,26 @@
-<?php
-
+<?
 	#
 	# $Id$
 	#
 
 	include("include/init.php");
 
-	if (! login_check_login()){
+
+	#
+	# do we have a valid cookie set?
+	#
+
+	if (!login_check_login()){
 
 		$GLOBALS['error']['badcookies'] = 1;
 		$smarty->display("page_checkcookie.txt");
-		exit();
+		exit;
 	}
+
+
+	#
+	# where shall we bounce to?
+	#
 
 	$redir = "/";
 
@@ -19,7 +28,11 @@
 		$redir = $alt_redir;
 	}
 
-	header("location: {$redir}");
-	exit();
 
+	#
+	# go!
+	#
+
+	header("location: {$redir}");
+	exit;
 ?>
