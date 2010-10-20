5bd17551a2e0ffd1db3fafc38729263ae2f5efd2
Cal Henderson <cal@iamcal.com>
2010-10-19 21:39:01 -0700

    overhaul account delete

--- account_delete.php
+++ account_delete.php
@@ -12,20 +12,7 @@
 
 	if (!login_check_login()){
 
-
-		#
-		#
-		#
-
-		if (get_isset('deleted')){
-
-			$smarty->assign('step', 'deleted');
-
-			$smarty->display('page_account_delete.txt');
-			exit;
-		}
-
-		header("location: /");
+		$smarty->display('page_account_delete_done.txt');
 		exit;
 	}
 
@@ -34,42 +21,32 @@
 	# generate a crumb
 	#
 
-	$new_crumb = crumb_generate_crumb($GLOBALS['cfg']['user']);
-	$smarty->assign('crumb', $new_crumb);
+	$crumb_key = 'account_delete';
+	$smarty->assign('crumb_key', $crumb_key);
 
 
 	#
 	# delete account?
 	#
 
-	if (post_str('delete')){
-
-		$crumb = post_str('crumb');
-
-		if (! crumb_validate_crumb($crumb, $GLOBALS['cfg']['user'])){
-
-			$GLOBALS['error']['badcrumb'] = 1;
-			$smarty->display('page_account_delete.txt');
-			exit();
-		}
+	if (post_str('delete') && crumb_check($crumb_key)){
 
 		if (post_str('confirm')){
 
 			$ok = users_delete_user($GLOBALS['cfg']['user']);
 
 			if ($ok){
-				login_do_logout('/account/delete?deleted=1');
-				exit();
+				login_do_logout('/account/delete/?deleted=1');
+				exit;
 			}
 
-			$smarty->assign('step', 'delete');
+			$smarty->assign('error_deleting', 1);
+
 			$smarty->display('page_account_delete.txt');
-			exit();
+			exit;
 		}
 
-		$smarty->assign('step', 'confirm');
-
-		$smarty->display('page_account_delete.txt');
+		$smarty->display('page_account_delete_confirm.txt');
 		exit;
 	}
 
@@ -77,8 +54,6 @@
 	#
 	# output
 	#
-
-	$smarty->assign('step', 'start');
 
 	$smarty->display("page_account_delete.txt");
 	exit;
