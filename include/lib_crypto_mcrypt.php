887579cf1d3b361daab6063bedca2b8ab68e1c83
Hubert Figuiere <hub@figuiere.net>
2011-03-02 00:01:16 -0800

    Set an initialization vector for mcrypt

--- include/lib_crypto_mcrypt.php
+++ include/lib_crypto_mcrypt.php
@@ -8,11 +8,17 @@
 
 	$GLOBALS['crypto_td'] = MCRYPT_RIJNDAEL_256;
 
+	function crypto_create_iv(){
+
+		$iv_size = mcrypt_get_iv_size($GLOBALS['crypto_td'], MCRYPT_MODE_ECB);
+		return mcrypt_create_iv($iv_size, MCRYPT_RAND);
+	}
+
 	#################################################################
 
 	function crypto_encrypt($data, $key){
 
-		$enc = mcrypt_encrypt($GLOBALS['crypto_td'], $key, $data, MCRYPT_MODE_ECB);
+		$enc = mcrypt_encrypt($GLOBALS['crypto_td'], $key, $data, MCRYPT_MODE_ECB, crypto_create_iv());
 		return base64_encode($enc);
 	}
 
@@ -21,7 +27,7 @@
 	function crypto_decrypt($enc_b64, $key){
 
 		$enc = base64_decode($enc_b64);
-		$dec = mcrypt_decrypt($GLOBALS['crypto_td'], $key, $enc, MCRYPT_MODE_ECB);
+		$dec = mcrypt_decrypt($GLOBALS['crypto_td'], $key, $enc, MCRYPT_MODE_ECB, crypto_create_iv());
 
 		return trim($dec);
 	}
