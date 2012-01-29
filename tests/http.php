48b00148c8949e0ff3891e12550542065e9ed852
Cal Henderson <cal@iamcal.com>
2012-01-29 15:34:33 -0800

    started moving tests to testmore

--- tests/http.php
+++ tests/http.php
@@ -3,7 +3,17 @@
 	# $Id$
 	#
 
-	include('../include/init.php');
+	include('testmore.php');
+	include('../www/include/init.php');
+
+	$ret = http_get("http://google.com");
+
+	is($ret['ok'], 0);
+	is($ret['code'], 302);
+	is_cmp(strlen($ret['headers']['location']), '>', 0);
+
+	#dumper($ret);
+	exit;
 
 	echo "<h1>http://google.com (should 304)</h1>";
 	dumper(http_get("http://google.com"));
