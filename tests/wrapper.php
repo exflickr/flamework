e9d46df3a11f76cb94da3cc465dc98fdcf7e3fc9
Cal Henderson <cal@iamcal.com>
2012-11-10 21:23:41 -0600

    modified test wrapper to provide its own log handler - this ensures that any errors/fatals go through diag() and wont mess up test results

--- tests/wrapper.php
+++ tests/wrapper.php
@@ -5,3 +5,21 @@
 	$dir = dirname(__FILE__);
 	include($dir.'/testmore.php');
 	include($dir.'/../www/include/init.php');
+
+	$GLOBALS['log_handlers']['error'] = array('test_wrapper');
+	$GLOBALS['log_handlers']['fatal'] = array('test_wrapper');
+
+	function _log_handler_test_wrapper($level, $msg, $more = array()){
+
+		$type = $more['type'] ? $more['type'] : $level;
+
+		$out = '';
+
+		if ($type) $out .= "[$type] ";
+
+		$out .= $msg;
+
+		if ($more['time'] > -1) $out .= " ($more[time] ms)";
+
+		diag($out);
+	}
