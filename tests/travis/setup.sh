43ad39ecd6751e23ba2735525ef6aeab5f224f1b
Cal Henderson <cal@iamcal.com>
2012-11-11 01:15:12 -0600

    added tests for the memcache module. start memcached within travis and instal the pecl extension

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -5,3 +5,4 @@ cp www/include/config.php.example www/include/config.php
 cat tests/travis/config.php >> www/include/config.php
 mysql -e 'CREATE DATABASE flamework;'
 mysql -Dflamework < schema/db_main.schema
+pecl install memcache
