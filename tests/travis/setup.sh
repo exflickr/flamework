7df516cc775e1ca49a7519e6453c420ee380c3a1
Cal Henderson <cal@iamcal.com>
2012-11-11 01:21:55 -0600

    try and avoid retarded pecl prompt that hangs testing. who designs stuff this bad, honestly?

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -5,4 +5,4 @@ cp www/include/config.php.example www/include/config.php
 cat tests/travis/config.php >> www/include/config.php
 mysql -e 'CREATE DATABASE flamework;'
 mysql -Dflamework < schema/db_main.schema
-pecl install memcache
+printf "\n" | pecl install memcache
