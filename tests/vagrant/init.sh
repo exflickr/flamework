30d9c926332e0c5c22739d48e39e29c97a47c192
Cal Henderson <cal@iamcal.com>
2013-05-19 17:44:23 -0700

    vagrant: install PHP_CodeCoverage vagrant: install xdebug

--- tests/vagrant/init.sh
+++ tests/vagrant/init.sh
@@ -17,3 +17,11 @@ cp www/include/config.php.example www/include/config.php
 cat tests/travis/config.php >> www/include/config.php
 mysql -e 'CREATE DATABASE flamework;'
 mysql -Dflamework < schema/db_main.schema
+
+apt-get install -y php-pear
+pear channel-discover pear.phpunit.de
+pear install phpunit/PHP_CodeCoverage
+
+apt-get install -y make
+pecl install xdebug
+echo "zend_extension=/usr/lib/php5/20090626+lfs/xdebug.so" > /etc/php5/conf.d/xdebug.ini
