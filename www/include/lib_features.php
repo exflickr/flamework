77b0f187764755a23b23582294a2bca6c1457c5c
straup <straup@gmail.com>
2013-09-09 07:58:44 -0400

    allow an array of features to be passed

--- www/include/lib_features.php
+++ www/include/lib_features.php
@@ -2,26 +2,47 @@
 
 	#################################################################
 
-	function features_is_enabled($flag){
+	function features_is_enabled($flags){
 
-		if (! preg_match("/^enable_feature_/", $flag)){
-			$flag = "enable_feature_{$flag}";
+		if (! is_array($flags)){
+			$flags = array($flags);
 		}
 
-		if (! isset($GLOBALS['cfg'][$flag])){
-			return 0;
+		foreach ($flags as $flag){
+
+			if (! preg_match("/^enable_feature_/", $flag)){
+				$flag = "enable_feature_{$flag}";
+			}
+
+			if (! isset($GLOBALS['cfg'][$flag])){
+				return 0;
+			}
+
+			if (! $GLOBALS['cfg'][$flag]){
+				return 0;
+			}
 		}
 
-		return $GLOBALS['cfg'][$flag];
+		return 1;
 	}
 
 	#################################################################
 
-	function features_ensure_enabled($flag){
+	function features_ensure_enabled($flags){
 
-		if (! features_is_enabled($flag)){
-			error_disabled();
+		if (! is_array($flags)){
+			$flags = array($flags);
 		}
+
+		foreach ($flags as $flag){
+			if (! features_is_enabled($flag)){
+				error_disabled();
+			}
+		}
+
+		# Happy sauce!
 	}
 
 	#################################################################
+
+	# the end
