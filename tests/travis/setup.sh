fda1e1a1b27ff0ee863f4eba3502ca1e9b8a04b3
Cal Henderson <cal@iamcal.com>
2013-09-30 16:54:45 -0700

    lrn2shell

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -6,7 +6,7 @@ chmod 755 www/templates_c
 cp www/include/config.php.example www/include/config.php
 
 cat tests/travis/config.php >> www/include/config.php
-if [ "$TRAVIS_PHP_VERSION" == "5.2" ]
+if [ "$TRAVIS_PHP_VERSION" == "5.2" ] then
 	cat tests/travis/config_52.php >> www/include/config.php
 fi
 
