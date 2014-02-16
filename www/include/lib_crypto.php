251585715e0f371bf567272466c4ff5d5c9ebb8a
Cal Henderson <cal@iamcal.com>
2014-02-16 13:39:38 -0800

    use log_fatal() for fatal errors. fail when an empty key is passed to encrypt. hash key using sha256, so key is as long as the block

--- www/include/lib_crypto.php
+++ www/include/lib_crypto.php
@@ -1,10 +1,14 @@
 <?php
-	if (!defined('MCRYPT_RIJNDAEL_256')) die("lib_crypto requires MCRYPT_RIJNDAEL_256");
-	if (!defined('MCRYPT_MODE_ECB')) die("lib_crypto requires MCRYPT_MODE_ECB");
+	if (!defined('MCRYPT_RIJNDAEL_256')) log_fatal("[lib_crypto] Flamework requires MCRYPT_RIJNDAEL_256");
+	if (!defined('MCRYPT_MODE_ECB')) log_fatal("[lib_crypto] Flamework requires MCRYPT_MODE_ECB");
 
 	#################################################################
 
 	function crypto_encrypt($data, $key){
+
+		if (!strlen($key)) log_fatal("[lib_crypto] Trying to encrypt with a blank key");
+
+		$key = hash('sha256', $key, true);
 
 		$enc = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $data, MCRYPT_MODE_ECB);
 		return base64_encode($enc);
@@ -13,6 +17,8 @@
 	#################################################################
 
 	function crypto_decrypt($enc_b64, $key){
+
+		if (strlen($key)) $key = hash('sha256', $key, true);
 
 		$enc = base64_decode($enc_b64);
 		$dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $enc, MCRYPT_MODE_ECB);
