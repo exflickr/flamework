521fc7874afca3463a23f40168872cec9b9f0266
Cal Henderson <cal@iamcal.com>
2014-02-16 13:31:58 -0800

    ECB does not use an IV. besides, if it did, you can't pass a random one to decrypt...

--- www/include/lib_crypto.php
+++ www/include/lib_crypto.php
@@ -1,22 +1,12 @@
 <?php
-
-	#################################################################
-
-	if (defined('MCRYPT_RIJNDAEL_256')){
-		$GLOBALS['crypto_td'] = MCRYPT_RIJNDAEL_256;
-	}
-
-	function crypto_create_iv(){
-
-		$iv_size = mcrypt_get_iv_size($GLOBALS['crypto_td'], MCRYPT_MODE_ECB);
-		return mcrypt_create_iv($iv_size, MCRYPT_RAND);
-	}
+	if (!defined('MCRYPT_RIJNDAEL_256')) die("lib_crypto requires MCRYPT_RIJNDAEL_256");
+	if (!defined('MCRYPT_MODE_ECB')) die("lib_crypto requires MCRYPT_MODE_ECB");
 
 	#################################################################
 
 	function crypto_encrypt($data, $key){
 
-		$enc = mcrypt_encrypt($GLOBALS['crypto_td'], $key, $data, MCRYPT_MODE_ECB, crypto_create_iv());
+		$enc = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $data, MCRYPT_MODE_ECB);
 		return base64_encode($enc);
 	}
 
@@ -25,7 +15,7 @@
 	function crypto_decrypt($enc_b64, $key){
 
 		$enc = base64_decode($enc_b64);
-		$dec = mcrypt_decrypt($GLOBALS['crypto_td'], $key, $enc, MCRYPT_MODE_ECB, crypto_create_iv());
+		$dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $enc, MCRYPT_MODE_ECB);
 
 		return trim($dec);
 	}
