c5a838706a649a05459b14aa8f45706bfa942243
straup <straup@gmail.com>
2014-04-26 19:20:39 +0200

    massage all the signup/in forms for bootstrap3

--- www/index.php
+++ www/index.php
@@ -1,28 +1,7 @@
 <?php
 	include('include/init.php');
 
+	$GLOBALS['smarty']->display('page_index.txt');
+	exit();
 
-	#
-	# this is so we can test the logging output
-	#
-
-	if ($_GET['log_test']){
-		log_error("This is an error!");
-		log_fatal("Fatal error!");
-	}
-
-
-	#
-	# this is so we can test the HTTP library
-	#
-
-	if ($_GET['http_test']){
-		$ret = http_get("http://google.com");
-	}
-
-
-	#
-	# output
-	#
-
-	$smarty->display('page_index.txt');
+?>
